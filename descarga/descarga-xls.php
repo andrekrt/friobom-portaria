5e2b6f7dc8b3d2401da5308d9d42a88e5cc846a5
André Santos FRIOBOM <andrekrt1922@gmail.com>
2022-02-18 09:15:59 -0300

    add relatorio descarga

---
+++ descarga/descarga-xls.php
@@ -0,0 +1,83 @@
+<?php
+
+session_start();
+require("../conexao.php");
+
+$tipoUsuario = $_SESSION['tipousuario'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Planilha</title>
+    </head>
+    <body>
+        <?php
+        
+            if(isset($_SESSION['idusuario']) && empty($_SESSION['idusuario'])==false){
+
+                $arquivo = 'descarga.xls';
+
+                $html = '';
+                $html .= '<table border="1">';
+                $html .= '<tr>';
+                $html .= '<td class="text-center font-weight-bold"> ID </td>';
+                $html .= '<td class="text-center font-weight-bold"> Data e Hora de Chegada </td>';
+                $html .= '<td class="text-center font-weight-bold"> Data e Hora de Registro </td>';
+                $html .= '<td class="text-center font-weight-bold"> Fornecedor </td>';
+                $html .= '<td class="text-center font-weight-bold"> Transportadora </td>';
+                $html .= '<td class="text-center font-weight-bold"> Tipo de Frete </td>';
+                $html .= '<td class="text-center font-weight-bold"> Nome Motorista </td>';
+                $html .= '<td class="text-center font-weight-bold"> RG Motorista </td>';
+                $html .= '<td class="text-center font-weight-bold"> Contato Motorista </td>';
+                $html .= '<td class="text-center font-weight-bold"> Placa </td>';
+                $html .= '<td class="text-center font-weight-bold"> Chave NF </td>';
+                $html .= '<td class="text-center font-weight-bold"> Num NF </td>';
+                $html .= '<td class="text-center font-weight-bold"> Qtd de Volume</td>';
+                $html .= '<td class="text-center font-weight-bold"> Valor Descarga </td>';
+                $html .= '<td class="text-center font-weight-bold"> Forma de Pagamento</td>';
+                $html .= '<td class="text-center font-weight-bold"> Situação </td>';
+                $html .= '</tr>';
+
+                $sql = $db->query("SELECT * FROM descarga LEFT JOIN fornecedores ON descarga.fornecedor = fornecedores.idfornecedores LEFT JOIN transportadoras ON descarga.transportadora = transportadoras.idtransportadoras");
+                $dados = $sql->fetchAll();
+                foreach($dados as $dado){
+
+                    $html .= '<tr>';
+                    $html .= '<td>'.$dado['token']. '</td>';
+                    $html .= '<td>'. date("d/m/Y H:i", strtotime($dado['data_hora_chegada'])) . '</td>';
+                    $html .= '<td>'.date("d/m/Y H:i", strtotime($dado['data_hora_registro'])) . '</td>';
+                    $html .= '<td>'. $dado['nome_fornecedor']. '</td>';
+                    $html .= '<td>'.$dado['nome_transportadora']. '</td>';
+                    $html .= '<td>'.$dado['tipo_frete'] . '</td>';
+                    $html .= '<td>'.$dado['nome_motorista'] . '</td>';
+                    $html .= '<td>'.$dado['rg_motorista']. '</td>';
+                    $html .= '<td>'.$dado['contato_motorista']. '</td>';
+                    $html .= '<td>'.$dado['placa']. '</td>';
+                    $html .= '<td>'.$dado['chave_nf']. '</td>';
+                    $html .= '<td>'.$dado['num_nf']. '</td>';
+                    $html .= '<td>'.str_replace(".",",", $dado['qtd_volume'] ). '</td>';
+                    $html .= '<td>'.str_replace(".",",",$dado['valor_descarga']) . '</td>';
+                    $html .= '<td>'.$dado['forma_pagamento']. '</td>';
+                    $html .= '<td>'.$dado['situacao']. '</td>';
+                    $html .= '</tr>';
+
+                }
+
+                $html .= '</table>';
+
+                header('Content-Type: application/vnd.ms-excel');
+                header('Content-Disposition: attachment;filename="'.$arquivo.'"');
+                header('Cache-Control: max-age=0');
+                header('Cache-Control: max-age=1');
+
+                echo $html;
+
+            }
+        
+        ?>
+    </body>
+</html>
